248f6d15993b69cc2fc400890f2c6e7a4ca82806
rasummer <richard.summerhayes@typesafe.com>
2014-03-04 16:00:57 -0800

    end of day commit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,7 +3,8 @@ package controllers
 import play.api.mvc.{Action, Controller}
 
 object Application extends Controller {
-  def index = Action {
-    Ok(views.html.index("Hello Play Framework"))
-  }
+  def index = Action {Ok(views.html.index("Hello Play Framework"))}
+  def scalabilityShowOff = Action {Ok(views.html.scalability.showOff("Scalability Show Off"))}
+  def scalabilityGetProof = Action {Ok(views.html.scalability.getProof("Scalability Get Proof"))}
+  def scalabilityConvince = Action {Ok(views.html.scalability.convince("Scalability Convince"))}
 }
